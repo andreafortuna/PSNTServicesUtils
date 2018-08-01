f08a97235a833895890c58944f01201afedc8402
Andrea Fortuna <andrea@andreafortuna.org>
2018-08-01 11:37:22 +0200

    code cleaning

--- PSNTServicesUtils.psm1
+++ PSNTServicesUtils.psm1
@@ -1,15 +1,14 @@
 function Get-Service(
-    [string]$serviceName = $(throw "serviceName is required"), 
-    [string]$targetServer = $(throw "targetServer is required"))
+    [string]$serviceName = $(throw "serviceName is required!"), 
+    [string]$targetServer = $(throw "targetServer is required!"))
 {
-    $service = Get-WmiObject -Namespace "root\cimv2" -Class "Win32_Service" `
-        -ComputerName $targetServer -Filter "Name='$serviceName'" -Impersonation 3    
+    $service = Get-WmiObject -Namespace "root\cimv2" -Class "Win32_Service" -ComputerName $targetServer -Filter "Name='$serviceName'" -Impersonation 3    
     return $service
 }
 
 function Test-ServiceResult(
-    [string]$operation = $(throw "operation is required"), 
-    [object]$result = $(throw "result is required"), 
+    [string]$operation = $(throw "Operation is required!"), 
+    [object]$result = $(throw "Result is required!"), 
     [switch]$continueOnError = $false)
 {
     $retVal = -1
@@ -17,66 +16,38 @@ function Test-ServiceResult(
          
     if ($retVal -eq 0) {return}
      
-    $errorcode = 'Success,Not Supported,Access Denied,Dependent Services Running,Invalid Service Control'
-    $errorcode += ',Service Cannot Accept Control, Service Not Active, Service Request Timeout'
-    $errorcode += ',Unknown Failure, Path Not Found, Service Already Running, Service Database Locked'
-    $errorcode += ',Service Dependency Deleted, Service Dependency Failure, Service Disabled'
-    $errorcode += ',Service Logon Failure, Service Marked for Deletion, Service No Thread'
-    $errorcode += ',Status Circular Dependency, Status Duplicate Name, Status Invalid Name'
-    $errorcode += ',Status Invalid Parameter, Status Invalid Service Account, Status Service Exists'
-    $errorcode += ',Service Already Paused'
+    $errorcode = 'Success,Not Supported,Access Denied,Dependent Services Running,Invalid Service Control,Service Cannot Accept Control,Service Not Active,Service Request Timeout,Unknown Failure,Path Not Found,Service Already Running,Service Database Locked,Service Dependency Deleted,Service Dependency Failure,Service Disabled,Service Logon Failure,Service Marked for Deletion,Service No Thread,Status Circular Dependency,Status Duplicate Name,Status Invalid Name,Status Invalid Parameter,Status Invalid Service Account,Status Service Exists,Service Already Paused'
     $desc = $errorcode.Split(',')[$retVal]
      
-    $msg = ("{0} failed with code {1}:{2}" -f $operation, $retVal, $desc)
+    $msg = ("Operation {0} failed with code {1}:{2}" -f $operation, $retVal, $desc)
      
     if (!$continueOnError) { Write-Error $msg } else { Write-Warning $msg }        
 }
 
 function Install-Service(
-    [string]$serviceName = $(throw "serviceName is required"), 
-    [string]$targetServer = $(throw "targetServer is required"),
-    [string]$displayName = $(throw "displayName is required"),
-    [string]$physicalPath = $(throw "physicalPath is required"),
-    [string]$userName = $(throw "userName is required"),
+    [string]$serviceName = $(throw "serviceName is required!"), 
+    [string]$targetServer = $(throw "targetServer is required!"),
+    [string]$displayName = $(throw "displayName is required!"),
+    [string]$physicalPath = $(throw "physicalPath is required!"),
+    [string]$userName = $(throw "userName is required!"),
     [string]$password = "",
     [string]$startMode = "Automatic",
     [string]$description = "",
     [bool]$interactWithDesktop = $false
 )
 {
-    # can't use installutil; only for installing services locally
-    #[wmiclass]"Win32_Service" | Get-Member -memberType Method | format-list -property:*    
-    #[wmiclass]"Win32_Service"::Create( ... )        
-          
-    # todo: cleanup this section 
-    $serviceType = 16          # OwnProcess
-    $serviceErrorControl = 1   # UserNotified
+    $serviceType = 16         
+    $serviceErrorControl = 1   
     $loadOrderGroup = $null
     $loadOrderGroupDepend = $null
     $dependencies = $null
-     
-    # description?
-    $params = `
-        $serviceName, `
-        $displayName, `
-        $physicalPath, `
-        $serviceType, `
-        $serviceErrorControl, `
-        $startMode, `
-        $interactWithDesktop, `
-        $userName, `
-        $password, `
-        $loadOrderGroup, `
-        $loadOrderGroupDepend, `
-        $dependencies `
-          
-    $scope = new-object System.Management.ManagementScope("\\$targetServer\root\cimv2", `
-        (new-object System.Management.ConnectionOptions))
+
+    $params = $serviceName, $displayName, $physicalPath, $serviceType, $serviceErrorControl, $startMode, $interactWithDesktop, $userName, $password, $loadOrderGroup, $loadOrderGroupDepend, $dependencies           
+    
+	$scope = new-object System.Management.ManagementScope("\\$targetServer\root\cimv2", (new-object System.Management.ConnectionOptions))
     "Connecting to $targetServer"
     $scope.Connect()
-    $mgt = new-object System.Management.ManagementClass($scope, `
-        (new-object System.Management.ManagementPath("Win32_Service")), `
-        (new-object System.Management.ObjectGetOptions))
+    $mgt = new-object System.Management.ManagementClass($scope, (new-object System.Management.ManagementPath("Win32_Service")), (new-object System.Management.ObjectGetOptions))
       
     $op = "service $serviceName ($physicalPath) on $targetServer"    
     "Installing $op"
@@ -90,58 +61,56 @@ function Install-Service(
 }
 
 function Start-Service(
-    [string]$serviceName = $(throw "serviceName is required"), 
-    [string]$targetServer = $(throw "targetServer is required"))
+    [string]$serviceName = $(throw "serviceName is required!"), 
+    [string]$targetServer = $(throw "targetServer is required!"))
 {
     "Getting service $serviceName on server $targetServer"
     $service = Get-Service $serviceName $targetServer
     if (!($service.Started))
     {
-        "Starting service $serviceName on server $targetServer"
+        "Starting service $serviceName on server $targetServer..."
         $result = $service.StartService()
         Test-ServiceResult -operation "Starting service $serviceName on $targetServer" -result $result   
     }
 }
 
 function Stop-Service(
-    [string]$serviceName = $(throw "serviceName is required"), 
-    [string]$targetServer = $(throw "targetServer is required"))
+    [string]$serviceName = $(throw "serviceName is required!"), 
+    [string]$targetServer = $(throw "targetServer is required!"))
 {
-    "Getting service $serviceName on server $targetServer"
+    "Getting service $serviceName on server $targetServer..."
     $service = Get-Service $serviceName $targetServer
     if (($service.Started))
     {
-        "Stopping service $serviceName on server $targetServer"
+        "Stopping service $serviceName on server $targetServer..."
         $result = $service.StopService()
         Test-ServiceResult -operation "Stopping service $serviceName on $targetServer" -result $result   
     }
 }
 
 function Uninstall-Service(
-    [string]$serviceName = $(throw "serviceName is required"), 
-    [string]$targetServer = $(throw "targetServer is required"))
+    [string]$serviceName = $(throw "serviceName is required!"), 
+    [string]$targetServer = $(throw "targetServer is required!"))
 {
     $service = Get-Service $serviceName $targetServer
      
     if (!($service))
     { 
-        Write-Warning "Failed to find service $serviceName on $targetServer. Nothing to uninstall."
+        Write-Warning "Failed to find service $serviceName on $targetServer."
         return
-    }
-     
-    "Found service $serviceName on $targetServer; checking status"
+    }     
+    "Found service $serviceName on $targetServer; checking status..."
              
     if ($service.Started)
     {
-        "Stopping service $serviceName on $targetServer"
+        "Stopping service $serviceName on $targetServer..."
         #could also use Set-Service, net stop, SC, psservice, psexec etc.
         $result = $service.StopService()
         Test-ServiceResult -operation "Stop service $serviceName on $targetServer" -result $result
     }
      
-    "Attempting to uninstall service $serviceName on $targetServer"
+    "Attempting to uninstall service $serviceName on $targetServer..."
     $result = $service.Delete()
     Test-ServiceResult -operation "Delete service $serviceName on $targetServer" -result $result   
 }
 
-Export-ModuleMember -Function 'Get-Service'
